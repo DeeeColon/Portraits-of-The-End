f1cbf7e0332b73d381934820dc0c25d07ed8907f
DeeeColon <yologwama@icloud.com>
2025-04-17 22:25:40 +0200

    Code for Buttons
    Added code for Start Button and Exit button functionality

---
+++ Assets/Yolo/Scripts/mainMenuUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// code for loading scene is referenced from:
+// Title: Load a scene in Unity from a button!
+// Author: RigorMortisTortoise
+// Date: 17 April 2025
+// Code Version: (ask lecturers about this)
+// Availability https://youtube.com/shorts/qCKmtlKmRyQ?si=ho4moXh2v9St8vYt
+
+public class mainMenuUI : MonoBehaviour
+{
+
+    public void startGameBTN()
+    {
+        SceneManager.LoadScene("openingDialogueScene");
+    }
+
+// Code for exit button is referenced from:
+// Title: MAIN MENU in Unity (Best Menu Tutorial 2024)
+// Author: SpeedTutor
+// Date: 17 April 2025
+// Code Version: (seek above)
+// Availability: https://youtu.be/Cq_Nnw_Lmnl?si6_3a7wW6jbYKRq_d
+
+    public void ExitBTN()
+    {
+        Application.Quit();
+    }
+
+
+}
